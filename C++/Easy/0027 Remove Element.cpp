55b78a6471d86a4c82984eae914af2bd4c9ebfaf
DrDAN6770 <118630187+DrDAN6770@users.noreply.github.com>
2023-11-04 21:44:57 +0800

    erase remove O(n2) O(1), 2pointer O(n) O(1)

---
+++ C++/Easy/0027 Remove Element.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 2pointer O(n) O(1)
+class Solution1 {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // not find
+        if (find(nums.begin(), nums.end(), val) == nums.end())
+            return nums.size();
+
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (val != nums[i])
+                nums[k++] = nums[i];
+        }
+                    
+        return k;
+    }
+};
+
+// erase remove O(n2) O(1)
+class Solution2 {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // not find
+        if (find(nums.begin(), nums.end(), val) == nums.end())
+            return nums.size();
+
+        nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
+                    
+        return nums.size();
+    }
+};
+
+int main() {
+    Solution1 ans1;
+    Solution2 ans2;
+
+    vector<int> nums1 = {3,2,2,3};
+    vector<int> nums2 = {3,2,2,3};
+    int val = 3;
+
+    int res1 = ans1.removeElement(nums1, val);
+    int res2 = ans2.removeElement(nums2, val);
+
+    cout << res1 << endl; // 2
+    cout << res2 << endl; // 2
+
+    return 0;
+}
